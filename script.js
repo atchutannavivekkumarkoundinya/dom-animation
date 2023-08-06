76321a082372a5cc3b81fbc6b9eb664fdf1237c4
Vivek kumar koundinya <91937270+atchutannavivekkumarkoundinya@users.noreply.github.com>
2023-08-06 23:10:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function myMove() {
     const elem = document.getElementById("animate");   
     let pos = 0;
     clearInterval(id);
-    id = setInterval(frame, 1);
+    id = setInterval(frame, 5);
     function frame() {
       if (pos == 350) {
         clearInterval(id);
